77e2966eba9e3efc4f157ecf8a6566d8b7988268
Brian McPhillips <B.McPhillips89@gmail.com>
2022-08-01 15:10:25 -0400

    Characters rendered to screen

--- src/containers/HeyArnold/HeyArnold.jsx
+++ src/containers/HeyArnold/HeyArnold.jsx
@@ -1,1 +1,24 @@
+import React, { useState } from 'react';
+import { useEffect } from 'react/cjs/react.production.min';
+import CharacterList from '../../components/CharacterList/CharacterList';
+import Loading from '../../components/Loading/Loading';
 
+const HeyArnold = () => {
+  const [loading, setLoading] = useState(true);
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    fetch('https://hey-arnold-api.herokuapp.com/api/v1/characters')
+      .then(res => res.json())
+      .then(data => setData(data))
+      .finally(() => setLoading(false));
+  }, []);
+
+  if(loading) return <Loading />;
+  return (
+    <CharacterList data={data}/>
+  );
+
+};
+
+export default HeyArnold;
