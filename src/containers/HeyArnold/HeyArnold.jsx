44902b4d6cde811742326efd37c33401281268f7
Brian McPhillips <B.McPhillips89@gmail.com>
2022-08-03 22:09:29 -0400

    export fetch to services

--- src/containers/HeyArnold/HeyArnold.jsx
+++ src/containers/HeyArnold/HeyArnold.jsx
@@ -2,14 +2,14 @@ import React, { useState } from 'react';
 import { useEffect } from 'react/cjs/react.production.min';
 import CharacterList from '../../components/CharacterList/CharacterList';
 import Loading from '../../components/Loading/Loading';
+import { fetchCharacters } from '../../services/hey-arnold';
 
 const HeyArnold = () => {
   const [loading, setLoading] = useState(true);
   const [data, setData] = useState([]);
 
   useEffect(() => {
-    fetch('https://hey-arnold-api.herokuapp.com/api/v1/characters')
-      .then(res => res.json())
+    fetchCharacters()
       .then(data => setData(data))
       .finally(() => setLoading(false));
   }, []);
