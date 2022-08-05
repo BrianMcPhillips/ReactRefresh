9844561c25368cdaff1a352174d27fd4d05a4fc7
Brian McPhillips <B.McPhillips89@gmail.com>
2022-08-05 11:05:30 -0400

    Fixed typo

--- src/components/CharacterList/CharacterList.test.jsx
+++ src/components/CharacterList/CharacterList.test.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { render, cleanup } from '@testing-library/react';
 import CharacterList from './CharacterList';
-import { MemoryRouter } from "react-router-dom";
+import { MemoryRouter } from 'react-router-dom';
 
 describe('CharacterList component', () => {
   afterEach(() => cleanup());
