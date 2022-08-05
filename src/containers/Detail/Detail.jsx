83b7c51e1f30cb42c80b440267dce9eaade02c59
Brian McPhillips <B.McPhillips89@gmail.com>
2022-08-04 23:28:43 -0400

    Detail page

--- src/containers/Detail/Detail.jsx
+++ src/containers/Detail/Detail.jsx
@@ -1,1 +1,26 @@
+import React, { useState, useEffect } from 'react';
+import { useParams } from 'react-router-dom';
+import CharacterDetail from '../../components/CharacterDetail/CharacterDetail';
+import Loading from '../../components/Loading/Loading';
 
+const Detail = () => {
+  const [data, setData] = useState({});
+  const [loading, setLoading] = useState(true);
+
+  const { id } = useParams(); 
+  useEffect(() => {
+    fetch(`https://hey-arnold-api.herokuapp.com/api/v1/characters/${id}`)
+      .then(res => res.json())
+      .then(data => setData(data))
+      .finally(setLoading(false));
+  }, []);
+
+  if(loading) return <Loading />;
+  return (
+    <div>
+      <CharacterDetail {...data}/>
+    </div>
+  );
+};
+
+export default Detail;
