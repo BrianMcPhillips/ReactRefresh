057584dc84c95fd17bc5cebad8f51720138b41b9
Brian McPhillips <B.McPhillips89@gmail.com>
2022-08-04 23:15:09 -0400

    Character Detail component

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -1,6 +1,18 @@
 import React from 'react';
 import HeyArnold from '../../containers/HeyArnold/HeyArnold';
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch
+} from 'react-router-dom';
 
 export default function App() {
-  return <HeyArnold />;
+  return (
+    <Router>
+      <Switch>
+        <Route exact path="/" component={HeyArnold}/>
+        {/* <Route exact path="/detail:id" component={}/> */}
+      </Switch>
+    </Router>
+  );
 }
