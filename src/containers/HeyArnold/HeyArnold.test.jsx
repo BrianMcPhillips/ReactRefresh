b8cceccd3b77f8006511fcee04340e2b5ef2df9d
Brian McPhillips <B.McPhillips89@gmail.com>
2022-08-07 19:39:45 -0400

    Loading mock test passing

---
+++ src/containers/HeyArnold/HeyArnold.test.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import HeyArnold from './HeyArnold';
+import { fetchCharacters } from '../../services/hey-arnold.js';
+import { screen, render } from '@testing-library/react';
+import { findByTestId } from '@testing-library/react';
+import { MemoryRouter } from 'react-router-dom';
+
+jest.mock('../../services/hey-arnold.js');
+
+describe('Hey Arnold container', () => {
+  it('displays loading screen on load', () => {
+    fetchCharacters.mockResolvedValue([
+      {
+        '_id': '5da237699734fdcb7bef8f56',
+        'name': 'Miles Shortman',
+        'image': 'https://vignette.wikia.nocookie.net/heyarnold/images/2/2e/Miles.jpg/revision/latest/scale-to-width-down/243?cb=20101115140639'
+      },
+      {
+        '_id': '5da237699734fdcb7bef8f54',
+        'name': 'Mitzi',
+        'image': 'https://vignette.wikia.nocookie.net/heyarnold/images/d/d9/Mitzi.png/revision/latest/scale-to-width-down/310?cb=20171212070331'
+      }
+    ]);
+    render(
+      <MemoryRouter>
+        <HeyArnold />
+      </MemoryRouter>
+    );
+
+    const loading = screen.getByTestId('loading');
+
+    expect(loading).toHaveTextContent('Loading');
+  });
+});
--- src/services/hey-arnold.js
+++ src/services/hey-arnold.js
@@ -3,3 +3,8 @@ export const fetchCharacters = () => {
     .then(res => res.json());
 
 };
+
+export const fetchSingleCharacter = (id) => {
+  return  fetch(`https://hey-arnold-api.herokuapp.com/api/v1/characters/${id}`)
+    .then(res => res.json());
+};
